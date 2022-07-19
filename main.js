fc943bd9d38e1def4e4acd92c3a30128219f12a7
Pamela <pamelaeugeniamartin@gmail.com>
2022-07-19 15:52:44 -0300

    cambios

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const nombreh2 = document.getElementById("nombre")
 const precioh4 = document.getElementById("precio")
 const idPizza = document.getElementById("number")
 const buscar = document.getElementById("btn")
+const form = document.getElementById("form")
+
 
 let pizzas = [
-{id:1,nombre: "Jamón",Ingredientes: ["Jamón", "mozzarella", "aceitunas","Morrón"], Precio: "$590"},
-{id:2,nombre: "Rúcula y Crudo", Ingredientes: ["Jamón crudo", "mozzarella", "aceitunas negras","Rúcula"], Precio: "$800"},
+{id:1, nombre: "Jamón",Ingredientes: ["Jamón", "mozzarella", "aceitunas","Morrón"], Precio: "$590"},
+{id:2, nombre: "Rúcula y Crudo", Ingredientes: ["Jamón crudo", "mozzarella", "aceitunas negras","Rúcula"], Precio: "$800"},
 {id:3, nombre: "Tropical", Ingredientes: ["Jamón", "mozzarella", "Ananá"],Precio: "$750"},
 {id:4, nombre: "Palmitos", Ingredientes: ["Jamón", "mozzarella", "aceitunas negras","Palmito", "Salsa Golf"], Precio: "$800"},
 {id:5, nombre: "De la casa", Ingredientes: ["salmon ahumado", "mozzarella", "alcaparras"], Precio: "$900"},
@@ -13,17 +15,20 @@ let pizzas = [
 ]
 
 
-buscar.addEventListener('click', (e) =>{
-    e.preventDefault();
+buscar.addEventListener ('click', laPizza);
 
-    let seleccion = pizzas.map ((pizza) =>{pizza.id === idPizza});
-
-   if (seleccion) {
-       nombreh2.innerHTML = pizzas.nombre;
-       precioh4.innerHTML = pizzas.Precio;
-   } else {
-    nombreh2.innerHTML = `No hay pizza con este ID <br>`;
-    precioh4.innerHTML = ""; 
-   }
-})
-
+function laPizza () {
+    
+    nombreh2.innerHTML = "";
+    precioh4.innerHTML = "";
+    const respuestaAlBuscar = idPizza.value;
+    pizzas.forEach ((pizza) => {
+        if (pizza.id == respuestaAlBuscar) {
+            nombreh2.innerHTML = `La pizza seleccionada es <br> ${pizza.nombre}`;
+            precioh4.innerHTML = `Su precio es ${pizza.Precio}`;
+        }
+    })
+    if (respuestaAlBuscar > 7) {
+        nombreh2.innerHTML = `No hay pizza con este ID <br>`;
+    }
+}
